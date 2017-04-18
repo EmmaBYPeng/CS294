12c7dc9ae622bcb2905f5fe4aab266a063e1fafd
EmmaBYPeng <u3502350@connect.hku.hk>
2017-04-18 13:21:24 -0700

    Behavior cloning overfit simple regression model

--- hw1/bc/bc.py
+++ hw1/bc/bc.py
@@ -1,5 +1,47 @@
 import argparse
-import pickle
-import numpy as np
 import tensorflow as tf
+from proc_data import Data
+from bc_model import BC
 
+
+def create_model(session, params):
+    model = BC(optimizer=params["optimizer"], lr=params["lr"])
+    session.run(tf.initialize_all_variables())
+    return model
+
+
+def train(params):
+    pass
+
+
+def overfit(params):
+    num_small_data = 100
+    data = Data(params["data_file"], train_ratio=0.9, val_ratio=0.05)
+    small_data = data.get_small_dataset(num_data=num_small_data)
+
+    batch_size, num_epochs = params["batch_size"], params["num_epochs"]
+    num_batch_per_epoch = int((num_small_data - 1) / batch_size) + 1
+
+    with tf.Session() as sess:
+        model = create_model(sess, params)
+        batches = data.batch_iter(small_data, batch_size, num_epochs)
+        for i, (batch_x, batch_y) in enumerate(batches):
+            pred, loss = model.step(sess, batch_x, batch_y)
+            print("Epoch %04d step %08d loss %04f" % (i/num_batch_per_epoch, i, loss))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_file", type=str, default="../data/Hopper_data.pkl")
+    parser.add_argument("--num_epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=50)
+    parser.add_argument("--optimizer", type=str, default="adam")
+    parser.add_argument("--lr", type=float, default=5e-2)
+    parser.add_argument("--train", type=bool, default=False)
+    parser.add_argument("--overfit", type=bool, default=True)
+    args = vars(parser.parse_args())
+
+    if args["overfit"]:
+        overfit(args)
+    elif args["train"]:
+        train(args)
---
+++ hw1/bc/bc_model.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+
+
+class BC(object):
+    def __init__(self, optimizer, lr):
+        self.obs = tf.placeholder(tf.float32, [None, 11])
+        self.actions = tf.placeholder(tf.float32, [None, 3])
+
+        self.pred, self.parameters = self.network()
+        self.loss = self.get_loss()
+        self.optimizer = self.get_optimizer(optimizer, lr)
+
+    def get_optimizer(self, optimizer, lr):
+        print("Using %s optimizer" % optimizer)
+        if optimizer == "adam":
+            return tf.train.AdamOptimizer(lr).minimize(self.loss)
+        elif optimizer == "adagrad":
+            return tf.train.AdagradOptimizer(lr).minimize(self.loss)
+        else:
+            return tf.train.GradientDescentOptimizer(lr).minimize(self.loss)
+
+    def get_loss(self):
+        loss = tf.reduce_mean(tf.pow(self.pred - self.actions, 2)) / 2
+        return loss
+
+    def step(self, sess, batch_x, batch_y, is_train=True):
+        feed_dict = {self.obs: batch_x, self.actions: batch_y}
+        if is_train:
+            _, pred, loss = sess.run([self.optimizer, self.pred, self.loss], feed_dict=feed_dict)
+        else:
+            pred, loss = sess.run([self.pred, self.loss], feed_dict=feed_dict)
+        return pred, loss
+
+    def network(self):
+        """Simple two layer neural network"""
+        parameters = []
+        with tf.variable_scope("fc1"):
+            w_fc1 = tf.get_variable('Weight', [11, 128],
+                                    initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
+            b_fc1 = tf.get_variable('Bias', [128],
+                                    initializer=tf.constant_initializer(0), dtype=tf.float32)
+            z_fc1 = tf.add(tf.matmul(self.obs, w_fc1), b_fc1)
+            a_fc1 = tf.nn.relu(z_fc1)
+            parameters += [w_fc1, b_fc1]
+        with tf.variable_scope("fc2"):
+            w_fc2 = tf.get_variable('Weight', [128, 3],
+                                    initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
+            b_fc2 = tf.get_variable('Bias', [3],
+                                    initializer=tf.constant_initializer(0), dtype=tf.float32)
+            z_fc1 = tf.add(tf.matmul(a_fc1, w_fc2), b_fc2)
+            parameters += [w_fc2, b_fc2]
+        return z_fc1, parameters
+
